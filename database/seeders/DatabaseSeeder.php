b55ca9706515ffc23bbfa1e35d98f041e2cd256e
Anastasiya <agropina_90@mail.ru>
2023-09-20 16:27:36 +0300

    fix: изучила классы Factory и Seed в Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        //заготовка для проекта, например, при работе с фильтрами
+        Category::factory(20)->create();
+        $tags = Tag::factory(50)->create();
+        $posts = Post::factory(100)->create();
+
+        foreach ($posts as $post) {
+            $tagsIds = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsIds);
+        }
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
