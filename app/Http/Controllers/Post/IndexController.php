a967bce63d4ce70e351d2a0b2f8b9208a0c28afd
Anastasiya <agropina_90@mail.ru>
2023-09-21 11:49:19 +0300

    fix: + фильтрацию данных при методе getAll()

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers\Post;
 
-use App\Http\Controllers\Controller;
-use App\Models\Category;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Post;
-use App\Models\Tag;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    /**
+     * @throws BindingResolutionException
+     */
+    public function __invoke(FilterRequest $request)
     {
-        $posts = Post::paginate(10);
+        $data = $request->validated();
+
+        $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
+
+        $posts = Post::filter($filter)->paginate(10);
 
         return view('post.index', compact('posts'));
     }
